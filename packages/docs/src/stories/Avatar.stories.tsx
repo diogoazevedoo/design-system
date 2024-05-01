9a38fbf322f4969833eca9dae65ce32bae1ea556
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-01 18:22:13 +0100

    docs: add avatar component doc page

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/diogoazevedoo.png',
+    alt: 'Diogo Azevedo',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
