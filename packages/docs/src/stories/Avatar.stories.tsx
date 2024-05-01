f366369dbb74e7892697a8939d4db4eb12ac1c8f
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-01 22:15:52 +0100

    docs: add controls to stories

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/diogoazevedoo.png',
     alt: 'Diogo Azevedo',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -6,8 +6,31 @@ export default {
   component: Text,
 
   args: {
+    size: 'md',
     children:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi blanditiis asperiores, vel voluptas hic, possimus officia quae consequuntur minus illo neque, veniam recusandae ipsa expedita excepturi omnis repudiandae quod?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
